37706954092f7c98f792a1d7fa2a050c56f74d92
Sachin2634 <105629187+Sachin2634@users.noreply.github.com>
2024-12-05 15:53:51 +0530

    All Done

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-var rect = document.querySelector("#rect")
+var rect = document.querySelector(".rect")
 var main = document.querySelector("#main")
-main.addEventListener("mousemove", (e)=>{
-    crsr.style.left = e.x + "px"
-    crsr.style.top = e.y + "px"
+// main.addEventListener("mousemove", (e)=>{
+//     rect.style.left = e.x + "px"
+    
+// })
+
+window.addEventListener("mousemove", function(dets){
+    var xval = gsap.utils.mapRange(0,window.innerWidth,0.2*window.innerWidth,0.8*window.innerWidth, dets.clientX)
+
+    gsap.to('.rect', {
+        left: xval,
+        ease: Power3
+
+    })
 })
